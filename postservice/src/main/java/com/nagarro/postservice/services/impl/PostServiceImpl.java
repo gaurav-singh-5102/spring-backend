c49d6943bdc0a3d2956cc146a282fd2a82f3d74e
Gaurav Singh <gaurav.singh10@nagarro.com>
2024-02-05 17:20:12 +0530

    add service and exceptions

---
+++ postservice/src/main/java/com/nagarro/postservice/services/impl/PostServiceImpl.java
@@ -0,0 +1,46 @@
+package com.nagarro.postservice.services.impl;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.nagarro.postservice.dto.PostDTO;
+import com.nagarro.postservice.exceptions.InvalidPostException;
+import com.nagarro.postservice.models.Post;
+import com.nagarro.postservice.repository.PostRepository;
+import com.nagarro.postservice.services.PostService;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+    private Validator validator;
+
+    public PostServiceImpl(PostRepository postRepository, Validator validator) {
+        this.postRepository = postRepository;
+        this.validator = validator;
+    }
+
+    @Override
+    public Post createPost(PostDTO postDTO) throws InvalidPostException {
+        validatePost(postDTO);
+        Post post = new Post();
+        post.setAuthor(postDTO.getAuthor());
+        post.setContent(postDTO.getContent());
+        post.setHeading(postDTO.getHeading());
+        post.setCreatedAt(LocalDateTime.now());
+        return postRepository.save(post);
+    }
+
+    private void validatePost(PostDTO postDTO) throws InvalidPostException {
+        Errors errors = new BeanPropertyBindingResult(postDTO, "entity");
+        validator.validate(postDTO, errors);
+        if (errors.hasErrors()) {
+            throw new InvalidPostException(errors.getAllErrors());
+        }
+    }
+
+}
