994909cbbcdc7e2f6f6716279f4138310f5f4a63
Gaurav Singh <gaurav.singh10@nagarro.com>
2024-02-21 15:37:07 +0530

    Cleaned unused import

--- postservice/src/main/java/com/nagarro/postservice/controllers/PostController.java
+++ postservice/src/main/java/com/nagarro/postservice/controllers/PostController.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
