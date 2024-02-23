8c78fcab82e961086ee2ccfdd0aeb5a25b804c9d
Kanika-Mahajan27 <71025841+Kanika-Mahajan27@users.noreply.github.com>
2024-02-23 11:40:34 +0530

    Removed the unused constructor

--- chat/src/main/java/com/nagarro/websockets/exception/InvalidNotificationRequestException.java
+++ chat/src/main/java/com/nagarro/websockets/exception/InvalidNotificationRequestException.java
@@ -6,11 +6,7 @@ import org.springframework.validation.ObjectError;
 
 public class InvalidNotificationRequestException extends Exception {
 
-	public InvalidNotificationRequestException(String message ) {
-		super(message);
-	}
-
 	public InvalidNotificationRequestException(List<ObjectError> allErrors) {
-		// TODO Auto-generated constructor stub
+		super("Validation failed for Notification : " + allErrors);
 	}
 }
