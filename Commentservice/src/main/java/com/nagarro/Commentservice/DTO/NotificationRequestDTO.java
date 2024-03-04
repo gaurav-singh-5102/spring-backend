3fa86169e6d3f0777d95cf332105eb979aced5c6
Kanika-Mahajan27 <kanikamahajan702@gmail.com>
2024-03-04 20:08:54 +0530

    added notificatin service to comments

---
+++ Commentservice/src/main/java/com/nagarro/Commentservice/DTO/NotificationRequestDTO.java
@@ -0,0 +1,66 @@
+package com.nagarro.Commentservice.DTO;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class NotificationRequestDTO {
+
+	@NotBlank(message="Notification content cannot be null")
+	private String content;
+	@NotBlank(message="Sender content cannot be null")
+    private String sender;
+	@NotBlank(message="Receiver content cannot be null")
+    private String receiver;
+    private boolean isGroupNotification;
+    private LocalDateTime timestamp;
+    
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public boolean isGroupNotification() {
+		return isGroupNotification;
+	}
+	public void setGroupNotification(boolean isGroupNotification) {
+		this.isGroupNotification = isGroupNotification;
+	}
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	public NotificationRequestDTO(String content, String sender, String receiver, boolean isGroupNotification,
+			LocalDateTime timestamp) {
+		super();
+		this.content = content;
+		this.sender = sender;
+		this.receiver = receiver;
+		this.isGroupNotification = isGroupNotification;
+		this.timestamp = timestamp;
+	}
+	public NotificationRequestDTO() {
+		
+	}
+	@Override
+	public String toString() {
+		return "Notification [content=" + content + ", sender=" + sender + ", receiver=" + receiver
+				+ ", isGroupNotification=" + isGroupNotification + ", timestamp=" + timestamp + "]";
+	}
+	
+}
